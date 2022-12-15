05bdc62e73d01f9d4997b4082c8cec1911ecfae9
Ben Zhang <benzhangniu@gmail.com>
2022-12-15 18:06:11 -0500

    Don't use the true state in the detector

--- bsim/frontend/src/index.js
+++ bsim/frontend/src/index.js
@@ -145,9 +145,6 @@ function initializeEgoConfig() {
         ego.planner = BASIC_PLANNER_CONFIG.planner;
         ego.planner_options = BASIC_PLANNER_CONFIG.planner_options;
         ego.detector = 'l1_optimizer';
-        ego.detector_options = {
-            use_true_state: true
-        }
     }
 
     { // ego2
@@ -164,8 +161,7 @@ function initializeEgoConfig() {
         ego.target_speed = target_speed;
         ego.planner = BASIC_PLANNER_CONFIG.planner;
         ego.planner_options = BASIC_PLANNER_CONFIG.planner_options;
-        // ego.detector = 'none';
-        ego.detector = 'l1_optimizer';
+        ego.detector = 'none';
     }
 }
 
