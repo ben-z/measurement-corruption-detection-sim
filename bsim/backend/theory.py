72bdd91a71975d13e0b7c94183bef04490e35cf4
Ben Zhang <benzhangniu@gmail.com>
2023-02-09 11:55:23 -0500

    Support sensor availability vector in get_observability_matrix

--- bsim/backend/theory.py
+++ bsim/backend/theory.py
@@ -1,28 +1,44 @@
 import numpy as np
 from utils import powerset, get_observability_matrix as old_get_observability_matrix
 
-def get_observability_matrix(A, C, N):
+def get_observability_matrix(A, C, N, S):
     """
     Calculates the observability matrix of a system with observability index N.
+    Parameters:
+        A: state transition matrix
+        C: output matrix
+        N: observability index (number of time steps)
+        S: {0,1}^p vector indicating which sensors are available (1 means available, 0 means missing)
     """
 
     n = A.shape[0]
     p = C.shape[0]
 
+    # Matrix that sets the attacked sensors to zero
+    I_S = np.diag(S)
+
     O = np.zeros((p*N, n))
     for i in range(N):
-        O[i*p:(i+1)*p, :] = C @ np.linalg.matrix_power(A,i)
+        O[i*p:(i+1)*p, :] = I_S @ C @ np.linalg.matrix_power(A,i)
     
     return O
-
+    
 def main():
     # test that get_observability_matrix is equivalent to old_get_observability_matrix
     A = np.array([[1, 1], [0, 1]])
     C = np.array([[1, 0], [0, 1]])
     N = 2
-    O = get_observability_matrix(A, C, N)
-    O_old = old_get_observability_matrix(A, C, None, N)
-    assert np.allclose(O, O_old)
+    # available sensors
+    # R = list(range(C.shape[0]))
+    R = [0]
+    # available sensors as a vector
+    S = np.zeros(C.shape[0])
+    S[R] = 1
+
+    O = get_observability_matrix(A, C,  N, S)
+    O_old = old_get_observability_matrix(A, C, R, N)
+    # remove zero rows from O and compare it with O_old
+    assert np.allclose(O[~np.all(O == 0, axis=1)], O_old)
 
 if __name__ == "__main__":
     main()
