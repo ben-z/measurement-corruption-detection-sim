c4c9f51a2c3bcf2a8481e6125bb0e4c90a258ede
Ben Zhang <benzhangniu@gmail.com>
2023-10-20 17:21:09 -0700

    Introduce working attack detection

--- bsim_v2/main.py
+++ bsim_v2/main.py
@@ -31,10 +31,18 @@ from utils import (
     get_lookahead_idx,
     wrap_to_pi,
     clamp,
+    walk_trajectory_by_durations,
+    kinematic_bicycle_model_linearize,
+    calc_input_effects_on_output,
+    get_output_evolution_tensor,
+    get_state_evolution_tensor,
+    optimize_l0,
 )
 import matplotlib.pyplot as plt
 
 plt.rcParams['text.usetex'] = True
+np.set_printoptions(suppress=True)
+
 
 #%%
 
@@ -84,7 +92,7 @@ def calculate_segment_lengths(points: List[Tuple[float, float]]) -> List[float]:
 
 # path_points, path_headings, path_curvatures, path_dcurvatures = generate_figure_eight_approximation([0, 0], 10, 5, 1000)
 # path_points, path_headings, path_curvatures, path_dcurvatures = generate_circle_approximation([-10, 0], 10, 1000)
-path_points, path_headings, path_curvatures, path_dcurvatures = generate_figure_eight_approximation([0, 0], 2000, 1000, 1000)
+path_points, path_headings, path_curvatures, path_dcurvatures = generate_figure_eight_approximation([0, 0], 2000, 1000, 5000)
 
 path_lengths = calculate_segment_lengths(path_points)
 
@@ -107,25 +115,110 @@ velocity_profile = np.clip(velocity_profile_raw, min_linear_velocity, max_linear
 # velocity_profile = smooth_velocity_profile(velocity_profile, path_lengths, max_linear_acceleration)
 velocity_profile = np.clip(velocity_profile, min_linear_velocity, max_linear_velocity)
 
-# Plot the original velocity profile and the smoothed velocity profile
-plt.figure()
-plt.plot(velocity_profile, label='smoothed')
-plt.plot(velocity_profile_raw, label='original')
-plt.title('Velocity profile')
-plt.ylim([0, 1.1 * max(velocity_profile)])
-plt.legend()
-plt.show()
+# # Plot the original velocity profile and the smoothed velocity profile
+# plt.figure()
+# plt.plot(velocity_profile, label='smoothed')
+# plt.plot(velocity_profile_raw, label='original')
+# plt.title('Velocity profile')
+# plt.ylim([0, 1.1 * max(velocity_profile)])
+# plt.legend()
+# plt.show()
 
 # Plot the acceleration profile
 plt.figure()
 
+def estimate_state(output_hist, input_hist, estimate_hist, path_points, path_headings, velocities, Cs, dt, l, N, enable_fault_tolerance=True):
+    """
+    Estimates the state of the vehicle.
+    Parameters:
+        output_hist: list[float] - a list of N outputs
+        input_hist: list[float] - a list of N-1 inputs
+        estimate_hist: list[float] - a list of N-1 estimates
+        path_points: list[tuple[float,float]] - a list of points on the path
+        velocities: list[float] - a list of velocities at each point on the path
+        dt: float - the time step
+    Returns:
+        (x_hat, y_hat, theta_hat, v_hat, delta_hat): tuple[float,float,float,float,float] - the estimated state
+    """
+    output = output_hist[-1]
+    assert len(output) == 6, 'This function only works with output (x,y,theta,v,delta1,delta2)'
+
+    x_hat = output[0]
+    y_hat = output[1]
+    theta_hat = output[2]
+    v_hat = output[3]
+    delta_hat = np.mean([output[4], output[5]])
+
+    if not enable_fault_tolerance:
+        return x_hat,y_hat,theta_hat,v_hat,delta_hat
+
+    if len(output_hist) < N:
+        print(f"Insufficient data to solve for corrupt sensors. Need {N} outputs, only have {len(output_hist)}")
+        return x_hat,y_hat,theta_hat,v_hat,delta_hat
+
+    assert len(input_hist) == N-1, 'input_hist must be one shorter than output_hist'
+    assert len(estimate_hist) == N-1, 'estimate_hist must be one shorter than output_hist'
+    assert len(output_hist) == N, 'output_hist must be N long'
+
+    # Use the beginning of the time window and the path to generate the As
+    x0_hat = estimate_hist[0]
+    closest_idx = closest_point_idx(path_points, x0_hat[0], x0_hat[1])
+
+    # TODO: add in safe guard to prevent solving when we are too far away from the desired state
+    # We can use the oldest estimate to determine whether we are too far.
+    x_err = path_points[closest_idx][0] - x0_hat[0]
+    y_err = path_points[closest_idx][1] - x0_hat[1]
+    theta_err = wrap_to_pi(path_headings[closest_idx] - x0_hat[2])
+    v_err = velocities[closest_idx] - x0_hat[3]
+    delta_err = -x0_hat[4] # we use linear inerpolation, so the desrd delta is always 0
+
+    if abs(x_err) > 1.0 \
+            or abs(y_err) > 1.0 \
+            or abs(theta_err) > 0.1 \
+            or abs(v_err) > 3.0 \
+            or abs(delta_err) > 0.1:
+        print(f"Too far from desired state. x_err: {x_err:.2f}, y_err: {y_err:.2f}, theta_err: {theta_err:.2f}, v_err: {v_err:.2f}, delta_err: {delta_err:.2f}. Not solving.")
+        return x_hat,y_hat,theta_hat,v_hat,delta_hat
+
+    # Use walk_trajectory_by_durations to get the path indices at each time step
+    desired_path_indices = [closest_idx] + walk_trajectory_by_durations(path_points, velocities, closest_idx, [dt]*(N-1))
+
+    # Generate the As and Bs for each time step
+    models = [kinematic_bicycle_model_linearize(path_headings[idx], velocities[idx], 0, dt, l) for idx in desired_path_indices]
+    As = list(m[0] for m in models[:N-1])
+    Bs = list(m[1] for m in models[:N-1])
+    
+    # List of desired outputs at the linearization points
+    desired_trajectory = [C @ np.array([path_points[idx][0], path_points[idx][1], path_headings[idx], velocities[idx], 0]) for idx in desired_path_indices]
+    
+    # subtract the input effects from the output
+    input_effects = calc_input_effects_on_output(As, Bs, Cs, input_hist)
+    output_hist_no_input_effects = [output - input_effect - desired_output for output, input_effect, desired_output in zip(output_hist, input_effects, desired_trajectory)]
+
+    # Normalize angular measurements
+    for output in output_hist_no_input_effects:
+        output[2] = wrap_to_pi(output[2])
+        output[4] = wrap_to_pi(output[4])
+        output[5] = wrap_to_pi(output[5])
+
+    # Solve for corrupted sensors
+    Y = np.array(output_hist_no_input_effects)
+    Phi = get_output_evolution_tensor(Cs, get_state_evolution_tensor(As))
+
+    x0_hat, prob, metadata, solns = optimize_l0(Phi, Y, eps=np.array([0.5]*2+[1e-3]*4))
+    assert metadata is not None, 'Optimization failed'
+    print("K: ", metadata['K'])
+
+    return x_hat,y_hat,theta_hat,v_hat,delta_hat
 
 # Control the bicycle to follow the path
 simulation_seconds = 30
 num_steps = int(simulation_seconds / model_params['dt'])
 x0 = np.array([200,0, pi/4, 1, 0])
 state = x0
+N = 10
 state_hist = []
+output_hist = []
 estimate_hist = []
 u_hist = []
 closest_idx_hist = []
@@ -135,20 +228,24 @@ prev_closest_idx = None
 for i in range(num_steps):
     state_hist.append(state)
 
-    output = np.concatenate((state, [state[4]]))
+    C = np.array([
+        [1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 0, 1, 0],
+        [0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 1],
+    ])
+
+    # measurement
+    output = C @ state
     # attack
-    if i * model_params['dt'] > 5:
-        output[3] += 5
+    # if i * model_params['dt'] > 5:
+    #     output[3] += 5
+    output_hist.append(output)
 
     # fault-tolerant estimator
-    
-
-    # estimates
-    x_hat = output[0]
-    y_hat = output[1]
-    theta_hat = output[2]
-    v_hat = output[3]
-    delta_hat = np.mean([output[4], output[5]])
+    x_hat, y_hat, theta_hat, v_hat, delta_hat = estimate_state(output_hist[-N:], u_hist[-(N-1):], estimate_hist[-(N-1):], path_points, path_headings, velocity_profile, [C]*N, dt=model_params['dt'], l=model_params['l'], N=N, enable_fault_tolerance=False)
 
     estimate_hist.append([x_hat, y_hat, theta_hat, v_hat, delta_hat])
 
@@ -183,14 +280,14 @@ for i in range(num_steps):
     u_hist.append([a, delta_dot])
 t_hist = [i * model_params['dt'] for i in range(num_steps)]
 
-# Plot state_hist on top of path_points
-plt.figure()
-plt.plot([p[0] for p in path_points], [p[1] for p in path_points], '.', label='path')
-plt.plot([p[0] for p in state_hist], [p[1] for p in state_hist], '.', label='ego')
-plt.axis('equal')
-plt.title('BEV')
-plt.legend()
-plt.show()
+# # Plot state_hist on top of path_points
+# plt.figure()
+# plt.plot([p[0] for p in path_points], [p[1] for p in path_points], '.', label='path')
+# plt.plot([p[0] for p in state_hist], [p[1] for p in state_hist], '.', label='ego')
+# plt.axis('equal')
+# plt.title('BEV')
+# plt.legend()
+# plt.show()
 
 # Plot simulation data
 EGO_COLOR = 'tab:orange'
@@ -242,44 +339,57 @@ axes_control[1].set_ylabel(r'Steering rate ($rad/s$)')
 axes_control[1].legend()
 
 #%%
+res = estimate_state([o + np.array([0,0,0,0,0,0]) for o in output_hist[-N:]], u_hist[-(N-1):], estimate_hist[-(N-1):], path_points, path_headings, velocity_profile, [C]*N, dt=model_params['dt'], l=model_params['l'], N=N, enable_fault_tolerance=True)
+# TODO: handle wrapping of angular measurements in the optimization problem -- how come this is still an issue even though we've subtracted away
+# the linearization points?
+
+# %%
+
+# plt.show()
+
+#%%
 # Generate a GIF
-from matplotlib.animation import PillowWriter, FuncAnimation
-ANIM_TITLE_FORMAT = 'Simulation Playback (Current time: ${:.2f}$ s)'
+def generate_gif():
+    from matplotlib.animation import PillowWriter, FuncAnimation
+    ANIM_TITLE_FORMAT = 'Simulation Playback (Current time: ${:.2f}$ s)'
 
-# add time cursors
-time_cursors = []
-time_cursors.append(ax_velocity.axvline(0, color='k'))
-time_cursors.append(ax_heading.axvline(0, color='k'))
-time_cursors.append(axes_control[0].axvline(0, color='k'))
-time_cursors.append(axes_control[1].axvline(0, color='k'))
-def animate(i):
-    # ================= Update title =================
-    suptitle.set_text(ANIM_TITLE_FORMAT.format(i * model_params['dt']))
+    # add time cursors
+    time_cursors = []
+    time_cursors.append(ax_velocity.axvline(0, color='k'))
+    time_cursors.append(ax_heading.axvline(0, color='k'))
+    time_cursors.append(axes_control[0].axvline(0, color='k'))
+    time_cursors.append(axes_control[1].axvline(0, color='k'))
+    def animate(i):
+        # ================= Update title =================
+        suptitle.set_text(ANIM_TITLE_FORMAT.format(i * model_params['dt']))
 
-    # =============== Plot ego position ===============
-    # For a moving position, use this:
-    #ego_position.set_data(state_hist[i][0], state_hist[i][1])
-    # To plot the entire trajectory, use this:
-    ego_position.set_data([p[0] for p in state_hist[:i+1]], [p[1] for p in state_hist[:i+1]])
-    ego_position_estimate.set_data([p[0] for p in estimate_hist[:i+1]], [p[1] for p in estimate_hist[:i+1]])
+        # =============== Plot ego position ===============
+        # For a moving position, use this:
+        #ego_position.set_data(state_hist[i][0], state_hist[i][1])
+        # To plot the entire trajectory, use this:
+        ego_position.set_data([p[0] for p in state_hist[:i+1]], [p[1] for p in state_hist[:i+1]])
+        ego_position_estimate.set_data([p[0] for p in estimate_hist[:i+1]], [p[1] for p in estimate_hist[:i+1]])
 
-    # =============== Plot time cursors ===============
-    for time_cursor in time_cursors:
-        time_cursor.set_xdata([i * model_params['dt']])
+        # =============== Plot time cursors ===============
+        for time_cursor in time_cursors:
+            time_cursor.set_xdata([i * model_params['dt']])
 
-    return suptitle, ego_position, *time_cursors
+        return suptitle, ego_position, *time_cursors
 
-anim_interval_ms = 2000
-anim = FuncAnimation(fig, animate, frames=range(0, num_steps, int(anim_interval_ms / 1000 / model_params['dt'])), interval=anim_interval_ms)
+    anim_interval_ms = 2000
+    anim = FuncAnimation(fig, animate, frames=range(0, num_steps, int(anim_interval_ms / 1000 / model_params['dt'])), interval=anim_interval_ms)
 
-start = time.perf_counter()
-# anim.save('zero_state.gif', writer=PillowWriter(fps=1000/anim_interval_ms))
+    start = time.perf_counter()
+    # anim.save('zero_state.gif', writer=PillowWriter(fps=1000/anim_interval_ms))
 
-from IPython.core.display import HTML
-html = HTML(anim.to_jshtml())
-print(f"Animation generation took {time.perf_counter() - start:.2f} seconds")
-plt.close(fig)
-html
+    from IPython.core.display import HTML
+    html = HTML(anim.to_jshtml())
+    print(f"Animation generation took {time.perf_counter() - start:.2f} seconds")
+    plt.close(fig)
+
+    return html
+
+# generate_gif()
 
 # #%%
 
