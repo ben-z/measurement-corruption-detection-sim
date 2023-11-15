35db2931f0b5eb9720788353ba034571ad590f4f
Ben Zhang <ben.zhang@watonomous.ca>
2023-11-15 19:17:35 +0000

    Minor test cleanup

--- bsim_v2/test_fault_generators.py
+++ bsim_v2/test_fault_generators.py
@@ -10,30 +10,28 @@ from fault_generators import (
 
 class TestAttackGenerators(unittest.TestCase):
     def test_sensor_bias_fault(self):
-        generator = sensor_bias_fault(start_t=5, bias=1, sensor_idx=2)
+        generator = sensor_bias_fault(start_t=5, sensor_idx=2, bias=1)
         output = [0] * 6
         for t in range(10):
             output = generator(t, output)
         self.assertEqual(output[2], 1 * (10 - 5 - 1))
 
     def test_intermittent_fault(self):
-        generator = intermittent_fault(
-            start_t=4, anomaly_value=10, interval=2, sensor_idx=3
-        )
+        generator = intermittent_fault(start_t=4, sensor_idx=3, anomaly_value=10, interval=2)
         output = [0] * 6
         for t in range(10):
             output = generator(t, output)
         self.assertEqual(output[3], 10)
 
     def test_complete_failure(self):
-        generator = complete_failure(start_t=3, failure_value=0, sensor_idx=1)
+        generator = complete_failure(start_t=3, sensor_idx=1, failure_value=0)
         output = [0] * 6
         for t in range(10):
             output = generator(t, output)
         self.assertEqual(output[1], 0)
 
     def test_drift_fault(self):
-        generator = drift_fault(start_t=2, drift_rate=0.5, sensor_idx=4)
+        generator = drift_fault(start_t=2, sensor_idx=4, drift_rate=0.5)
         output = [0] * 6
         for t in range(10):
             output = generator(t, output)
@@ -41,16 +39,14 @@ class TestAttackGenerators(unittest.TestCase):
         self.assertAlmostEqual(output[4], expected_drift, places=3)
 
     def test_random_noise_fault(self):
-        generator = random_noise_fault(start_t=1, noise_level=5, sensor_idx=5)
+        generator = random_noise_fault(start_t=1, sensor_idx=5, noise_level=5)
         output = [0] * 6
         for t in range(10):
             output = generator(t, output)
         self.assertNotEqual(output[5], 0)
 
     def test_spike_fault(self):
-        generator = spike_fault(
-            start_t=6, spike_value=3, duration=2, sensor_idx=0
-        )
+        generator = spike_fault(start_t=6, sensor_idx=0, spike_value=3, duration=2)
         output = [0] * 6
         for t in range(10):
             output = generator(t, output)
