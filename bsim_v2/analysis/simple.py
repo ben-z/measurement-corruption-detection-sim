8a4d01622a2cd459155527e318476493e2164e52
Ben Zhang <ben.zhang@watonomous.ca>
2023-12-27 23:02:40 +0000

    Enable a simple analysis

--- bsim_v2/analysis/simple.py
+++ bsim_v2/analysis/simple.py
@@ -69,10 +69,10 @@ exp_path = Path(__file__).parent.parent.parent / "exp"
 # fault_name = "Drift Rate"
 # exp_names = []
 
-# file_path = exp_path / "test-drift-sensors-4-5.jsonl"
-# fault_conf_column = "fault_spec.kwargs.drift_rate"
-# fault_name = "Drift Rate"
-# exp_names = []
+file_path = exp_path / "test-drift-sensors-4-5.jsonl"
+fault_conf_column = "fault_spec.kwargs.drift_rate"
+fault_name = "Drift Rate"
+exp_names = []
 
 
 # %%
